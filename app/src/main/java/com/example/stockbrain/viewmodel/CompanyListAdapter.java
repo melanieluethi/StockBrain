b509636494b008beb7a9ca332f0aaff6dbd6f942
melanieluethi <melanie_luethi@hotmail.de>
2021-05-03 22:00:47 +0200

    fix start problems

--- app/src/main/java/com/example/stockbrain/viewmodel/CompanyListAdapter.java
+++ app/src/main/java/com/example/stockbrain/viewmodel/CompanyListAdapter.java
@@ -10,4 +10,12 @@ public class CompanyListAdapter extends BaseObservable implements ListItemIntera
     public void onItemClick(View view, int position) {
 
     }
+
+    public void getCompanyDetails(String ticker) {
+        // TODO LUM: handling when which methode is called and convert to dao
+        CompanyDetailsGetAdapter.getCompanyGeneral(ticker);
+        CompanyDetailsGetAdapter.getCompanyPrices(ticker);
+        CompanyDetailsGetAdapter.getCompanyStatements(ticker);
+    }
+
 }
