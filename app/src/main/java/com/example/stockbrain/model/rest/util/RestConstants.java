5e49ee965ba8791c1418c744d8b2ee7a5a67c7aa
melanieluethi <melanie_luethi@hotmail.de>
2021-05-21 15:47:43 +0200

    correct to actual data not to take 2014

--- app/src/main/java/com/example/stockbrain/model/rest/util/RestConstants.java
+++ app/src/main/java/com/example/stockbrain/model/rest/util/RestConstants.java
@@ -1,11 +1,13 @@
 package com.example.stockbrain.model.rest.util;
 
+import java.util.Calendar;
+
 public class RestConstants {
     protected final static String BASE_URL_STOCK = "https://simfin.com";
     protected final static String API_KEY_STOCK = "VVSzN6wGL8gKtyZyXt1OlrdzxJs9JpaH";
     public final static String STATEMENT_BALANCE_SHEET = "bs";
     public final static String STATEMENT_PROFIT_LOSS = "pl";
     public final static String PERIOD = "q1";
-    public final static Integer FYEAR = 2020;
+    public final static Integer FYEAR = Calendar.getInstance().get(Calendar.YEAR);;
     protected final static String BASE_URL_LOGO = "https://autocomplete.clearbit.com";
 }
